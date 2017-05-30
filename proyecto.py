4832716722d1d7d0e6b40449b6a15e4e6c4cf2e1
edgarosovel <edgarov@live.com.mx>
2017-05-30 18:17:56 -0500

    Graphs

--- proyecto.py
+++ proyecto.py
@@ -2,10 +2,12 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from functools import reduce
 import matplotlib.pyplot as plt
 
 #Leer datos
 datos = pd.read_csv("datos/estudiantes_merged.csv",sep=",")
+muestra = pd.read_csv("datos/muestra.csv",sep=",")
 
 #por = pd.read_csv("datos/student-por.csv",sep=",")
 #datos = [mat,por]
@@ -25,14 +27,14 @@ datos = pd.read_csv("datos/estudiantes_merged.csv",sep=",")
 # target.close()
 
 
-
-calis = plt.figure(1)
+######## HISTOGRAMAS CALIFICACIONES POBLACIÓN
+p_calis = plt.figure(1)
 plt.subplot(2, 2, 1)
-plt.tight_layout()
+# plt.tight_layout()
 plt.xlabel('Calificación')
 plt.ylabel('# de alumnos')
-plt.hist((datos["G1"]*0.5),facecolor='g') # *0.5 porque las calis están sobre 20
-plt.text(2, 100, r'$\mu=100,\ \sigma=15$')
+plt.hist((datos["G1"]*0.5),bins=range(11)) # *0.5 porque las calis están sobre 20
+#plt.text(2, 100, r'$\mu=100,\ \sigma=15$')
 plt.grid(True)
 plt.title("Primer parcial")
 #
@@ -40,7 +42,7 @@ plt.subplot(2, 2, 2)
 plt.xlabel('Calificación')
 plt.ylabel('# de alumnos')
 plt.tight_layout()
-plt.hist(datos["G2"]*0.5)
+plt.hist(datos["G2"]*0.5,bins=range(11))
 plt.grid(True)
 plt.title("Segundo parcial")
 #
@@ -48,37 +50,108 @@ plt.subplot(2, 2, 3)
 plt.xlabel('Calificación')
 plt.ylabel('# de alumnos')
 plt.tight_layout()
-plt.hist(datos["G3"]*0.5)
+plt.hist(datos["G3"]*0.5,bins=range(11))
 plt.grid(True)
 plt.title("Tercer parcial")
 #
 plt.subplot(2, 2, 4)
 plt.xlabel('Calificación')
 plt.ylabel('# de alumnos')
-plt.tight_layout()
-plt.hist(((datos["G1"]*0.5)+(datos["G2"]*0.5)+(datos["G3"]*0.5))/3)
+#plt.tight_layout()
+plt.hist(((datos["G1"]*0.5)+(datos["G2"]*0.5)+(datos["G3"]*0.5))/3,facecolor='g',bins=range(11))
 plt.grid(True)
 plt.title("Calificación final")
 #
-calis.show()
+print("Media: ",(reduce(lambda x,y: x+y,((datos["G1"]*0.5)+(datos["G2"]*0.5)+(datos["G3"]*0.5))/3))/len(datos))
+p_calis.show()
 
-alcohol_calis = plt.figure(2)
 
+######## HISTOGRAMAS CALIFICACIONES MUESTRA
+m_calis = plt.figure(4)
+plt.subplot(2, 2, 1)
+plt.tight_layout()
+plt.xlabel('Calificación')
+plt.ylabel('# de alumnos')
+plt.hist((muestra["G1"]*0.5),bins=range(11)) # *0.5 porque las calis están sobre 20
+#plt.text(2, 100, r'$\mu=100,\ \sigma=15$')
+plt.grid(True)
+plt.title("Primer parcial M")
+#
+plt.subplot(2, 2, 2)
+plt.xlabel('Calificación')
+plt.ylabel('# de alumnos')
+plt.tight_layout()
+plt.hist(muestra["G2"]*0.5,bins=range(11))
+plt.grid(True)
+plt.title("Segundo parcial M")
+#
+plt.subplot(2, 2, 3)
+plt.xlabel('Calificación')
+plt.ylabel('# de alumnos')
+plt.tight_layout()
+plt.hist(muestra["G3"]*0.5,bins=range(11))
+plt.grid(True)
+plt.title("Tercer parcial M")
+#
+plt.subplot(2, 2, 4)
+plt.xlabel('Calificación')
+plt.ylabel('# de alumnos')
+plt.tight_layout()
+res=plt.hist(((muestra["G1"]*0.5)+(muestra["G2"]*0.5)+(muestra["G3"]*0.5))/3,facecolor='g',bins=range(11))
+plt.grid(True)
+plt.title("Calificación final M")
+
+#np.histogram()
+#
+print("Tabla de Calificación final (Muestra)")
+print("Media: ",(reduce(lambda x,y: x+y,((muestra["G1"]*0.5)+(muestra["G2"]*0.5)+(muestra["G3"]*0.5))/3))/len(muestra))
+x=0
+suma=0
+for i in res[0]:
+	suma+=i
+	print("["+str(res[1][x])+","+str(res[1][x+1])+"] | "+str(i)+" | "+str(suma)+" | "+str())
+	x+=1
+m_calis.show()
+
+
+######## GRÁFICAS CONSUMO DE ALCOHOL POBLACIÓN
+p_alcohol_calis = plt.figure(2)
 plt.subplot(2, 1, 1)
 plt.xlabel('Nivel de consumo')
 plt.ylabel('# de alumnos')
 plt.tight_layout()
-plt.hist(datos["Walc"])
+plt.hist((datos['Walc']+datos['Dalc']),facecolor='pink',bins=range(11))
 plt.grid(True)
-plt.title("Consumo de alcohol (Fin de semana)")
+plt.title("Consumo de alcohol (Población)")
 
 plt.subplot(2, 1, 2)
 plt.xlabel('Calificación')
 plt.ylabel('Nivel de alcoholismo')
-plt.scatter(((datos["G1"]*0.5)+(datos["G2"]*0.5)+(datos["G3"]*0.5))/3,(datos['Walc']+datos['Dalc']))
+plt.scatter(((datos["G1"]*0.5)+(datos["G2"]*0.5)+(datos["G3"]*0.5))/3,(datos['Walc']+datos['Dalc']),facecolor='r')
 #plt.scatter((datos["G1"]*0.5),(datos['Walc']+datos['Dalc']))
-plt.title("Relación: Consumo de alcohol - Calificaciones")
+plt.title("Dispersión: Consumo de alcohol - Calificaciones")
 
-alcohol_calis.show()
+p_alcohol_calis.show()
+
+
+######## GRÁFICAS CONSUMO DE ALCOHOL MUESTRA
+m_alcohol_calis = plt.figure(3)
+plt.subplot(2, 1, 1)
+plt.xlabel('Nivel de consumo')
+plt.ylabel('# de alumnos')
+plt.tight_layout()
+plt.hist((muestra['Walc']+muestra['Dalc']),facecolor='pink',bins=range(11))
+# plt.scatter((muestra['Walc']+muestra['Dalc']),facecolor='black')
+plt.grid(True)
+plt.title("Consumo de alcohol (Muestra)")
+
+plt.subplot(2, 1, 2)
+plt.xlabel('Calificación')
+plt.ylabel('Nivel de alcoholismo')
+plt.scatter(((muestra["G1"]*0.5)+(muestra["G2"]*0.5)+(muestra["G3"]*0.5))/3,(muestra['Walc']+muestra['Dalc']),facecolor='r')
+#plt.scatter((datos["G1"]*0.5),(datos['Walc']+datos['Dalc']))
+plt.title("Dispersión: Consumo de alcohol - Calificaciones")
+
+m_alcohol_calis.show()
 
 input()
